66ccab93c6f4e72b44003eb116e88a37f612237f
murata <murata.small.baseball@gmail.com>
2021-07-06 20:51:47 +0900

    computedのgetter,setter記載

--- src/main/resources/static/js/property.js
+++ src/main/resources/static/js/property.js
@@ -1,16 +1,16 @@
 const app = new Vue({
 	el: '#app',
 	data: {
-		message: 'Hello Vue.js'
+		basePrice: 100
 	},
 	computed: {
-		reversedMessage: function(){
-			return this.message.split('').reverse().join('')
-		}
-	},
-	methods: {
-		reverseMethod: function(){
-			return this.message.split('').reverse().join('')
+		taxIncludedPrice: {
+			get: function() {
+				return parseInt(this.basePrice * 1.08)
+			},
+			set: function(taxIncludedPrice){
+				this.basePrice = Math.ceil(taxIncludedPrice / 1.08)
+			}
 		}
 	}
 })
