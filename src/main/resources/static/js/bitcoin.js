dea66ff60a7dc7ebf844d8f186814e955fb54f6a
murata <murata.small.baseball@gmail.com>
2021-07-07 21:50:46 +0900

    api通信

--- src/main/resources/static/js/bitcoin.js
+++ src/main/resources/static/js/bitcoin.js
@@ -1,7 +1,9 @@
 const app = new Vue({
 	el: '#app',
 	data: {
-		bpi: null
+		bpi: null,
+		hasError: false,
+		loading: true
 	},
 	mounted: function(){
 		axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
@@ -11,6 +13,15 @@ const app = new Vue({
 		}.bind(this))
 		.catch(function(error){
 			console.log(error)
-		})
+			this.hasError = true
+		}.bind(this))
+		.finally(function(){
+			this.loading = false
+		}.bind(this))
+	},
+	filters: {
+		currencyDecimal(value) {
+			return value.toFixed(2)
+		}
 	}
 })
